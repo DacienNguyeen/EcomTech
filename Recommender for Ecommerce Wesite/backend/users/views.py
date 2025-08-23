9ef841e1723bdbd9a4094a8e2b6622ea456d4415
DacienNguyeen <daciennguyeen1904@gmail.com>
2025-08-23 14:50:40 +0700

    aaa

--- Recommender for Ecommerce Wesite/backend/users/views.py
+++ Recommender for Ecommerce Wesite/backend/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .models import UserProfile
+from .serializers import UserSerializer, UserProfileSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+	queryset = UserProfile.objects.all()
+	serializer_class = UserProfileSerializer
